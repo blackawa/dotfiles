f625fa5a3dd2aacfa5bb7d8e776e01010a296451
blackawa <3104kurokawa@gmail.com>
2016-07-03 19:02:59 +0900

    add plugins for emacs

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,8 +1,14 @@
+(require 'cask "/usr/local/Cellar/cask/0.7.4/cask.el")
+(cask-initialize)
+(require 'pallet)
+(require 'init-loader)
+(pallet-mode t)
+
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
-; do not create backup files(e.g, *~, .#*)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
