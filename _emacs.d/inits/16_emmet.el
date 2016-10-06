f601863f904cbfe7dc4c64beeaae7bcc06f5677b
blackawa <3104kurokawa@gmail.com>
2016-10-06 14:28:10 +0900

    キーバインドがうまく効いてなくて邪魔なのでけす

--- _emacs.d/inits/16_emmet.el
+++
@@ -1,9 +0,0 @@
-(require 'emmet-mode)
-
-(add-hook 'sgml-mode-hook 'emmet-mode)  ;; マークアップ言語全部で使う
-(add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
-(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
-(eval-after-load "emmet-mode"
-                   '(define-key emmet-mode-keymap (kbd "C-j") nil)) ;; C-j は newline のままにしておく
-(keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
-(define-key emmet-mode-keymap (kbd "H-i") 'emmet-expand-line) ;; C-i で展開
