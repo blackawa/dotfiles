866d072c9b1406b68ada47ad4aba6defbb1cbf83
blackawa <3104kurokawa@gmail.com>
2017-04-11 14:32:36 +0900

    add yaml mode

--- _emacs.d/inits/17_yaml-mode.el
+++ _emacs.d/inits/17_yaml-mode.el
@@ -1,3 +1,4 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.reek$" . yaml-mode))
 (define-key yaml-mode-map "\C-m" 'newline-and-indent)
