365f485bddbe46eb32ef7a5297142e33a2745599
blackawa <3104kurokawa@gmail.com>
2017-11-14 11:37:43 +0900

    add some packages to emacs

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -4,6 +4,10 @@
   (cask-initialize))
 (package-initialize)
 
+;;; Variables:
+(setq make-backup-files nil)
+
+;;; Use packages:
 (require 'use-package)
 (use-package helm :defer t
   :diminish helm-mode
@@ -15,7 +19,26 @@
 (use-package paredit :defer t
   :init
   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
+  :init
+  (setq markdown-command "multimarkdown")
+  (add-hook 'markdown-mode-hook 'enable-vmd-mode))
 
-;;; Key config
+;;; Key config:
 (progn
   (bind-key "C-h" 'delete-backward-char))
+
+;; custom-set-variables was added by Custom.
+(custom-set-variables
+ '(safe-local-variable-values
+   (quote
+    ((cider-refresh-after-fn . "integrant.repl/resume")
+     (cider-refresh-before-fn . "integrant.repl/suspend")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ )
