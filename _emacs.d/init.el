1354cf813070fb588455fea16c13d1c428db82bb
blackawa <3104kurokawa@gmail.com>
2017-11-14 19:18:51 +0900

    add keybinds like spacemacs

--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -9,14 +9,16 @@
 
 ;;; Use packages:
 (require 'use-package)
-(use-package helm :defer t
+(use-package helm
+  :defer t
   :diminish helm-mode
   :init
   (require 'helm-config)
   (bind-key "C-x C-f" 'helm-find-files)
   (bind-key "M-x" 'helm-smex)
   (helm-mode t))
-(use-package paredit :defer t
+(use-package paredit
+  :defer t
   :init
   (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode))
 (use-package markdown-mode
@@ -29,8 +31,24 @@
   (setq markdown-command "multimarkdown"))
 
 ;;; Key config:
-(progn
-  (bind-key "C-h" 'delete-backward-char))
+(bind-key "C-h" 'delete-backward-char)
+
+(defvar spacemaps (make-sparse-keymap) "Spacemacsを真似したkeymap")
+(defalias 'spacemaps-prefix spacemaps)
+(bind-key "M-SPC" 'spacemaps-prefix)
+
+(defvar filemaps (make-sparse-keymap) "ファイル操作のkeymap")
+(defalias 'filemaps-prefix filemaps)
+(bind-key "f" 'filemaps-prefix spacemaps)
+(bind-keys :map filemaps
+	   ("f" . find-file)
+	   ("l" . load-file))
+
+(defvar stringmaps (make-sparse-keymap) "文字列操作のkeymap")
+(defalias 'stringmaps-prefix stringmaps)
+(bind-key "s" 'stringmaps-prefix spacemaps)
+(bind-keys :map stringmaps
+	   ("i" . indent-region))
 
 ;; custom-set-variables was added by Custom.
 (custom-set-variables
